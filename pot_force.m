242faf6590dd1e4db92a23f3adf0bc115a6ec2ce
lcademartiri <ludovico.cademartiri@unipr.it>
2025-12-20 14:49:35 +0100

    data analysis completing

--- pot_force.m
+++ pot_force.m
@@ -3,14 +3,14 @@ function H=pot_force(potentialtype,cutoff,points,eqdistance,welldepth)
     if potentialtype==1 % Lennard Jones
         x=linspace(0,cutoff,points)';
         H(:,1)=x;
-        sigma11=(eqdistance)/(2^(1/6));
+        sigma11=(eqdistance);
         xtilde=x/sigma11;
         Ftilde = 24 .* (2 ./ xtilde.^13 - 1 ./ xtilde.^7);
         H(:,2) = (welldepth ./ sigma11) .* Ftilde;
         H(1,:)=[];   
     elseif potentialtype==2 % Weeks Chandler Andersen
         x = linspace(0, cutoff, points)';
-        sigma11 = (eqdistance)/(2^(1/6));
+        sigma11 = (eqdistance);
         xtilde = x ./ sigma11;
         Ftilde = 24 .* (2 ./ xtilde.^13 - 1 ./ xtilde.^7);  % correct derivative
         Ftilde(xtilde >= 2^(1/6)) = 0;  % truncate at r_min
